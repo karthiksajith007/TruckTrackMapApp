00c849813ee2642a8ec779ef684afc2523f6645c
Karthik Ajith <karthik@semcoisc.com>
2017-12-21 11:26:13 +0530

    Week referencing to avoid listener memory leak.

--- TruckMap/app/src/main/java/code/truckmap/com/truckmap/HttpRequestThread.java
+++ TruckMap/app/src/main/java/code/truckmap/com/truckmap/HttpRequestThread.java
@@ -4,6 +4,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 
 /**
  * Created by karthik on 12/21/2017.
@@ -15,12 +16,12 @@ public class HttpRequestThread extends Thread {
         void onHttpResponse (JSONObject responseJson, int requestID);
     }
 
-    private IHttpRequestThread iHttpRequestThread;
+    private WeakReference <IHttpRequestThread> iHttpRequestThreadRef;
     private String url;
     private int requestID;
 
     public HttpRequestThread (IHttpRequestThread iHttpRequestThread, String url, int requestID) {
-        this.iHttpRequestThread = iHttpRequestThread;
+        this.iHttpRequestThreadRef = new WeakReference<IHttpRequestThread>(iHttpRequestThread);
         this.url = url;
         this.requestID = requestID;
     }
@@ -42,8 +43,18 @@ public class HttpRequestThread extends Thread {
             e.printStackTrace();
             responseJson = null;
         }
+
+        IHttpRequestThread iHttpRequestThread = getListener ();
         if (iHttpRequestThread!=null) {
             iHttpRequestThread.onHttpResponse(responseJson, requestID);
         }
+    }
+
+    private IHttpRequestThread getListener () {
+        IHttpRequestThread iHttpRequestThread = null;
+        if (iHttpRequestThreadRef != null) {
+            iHttpRequestThread = iHttpRequestThreadRef.get();
+        }
+        return iHttpRequestThread;
     }
 }
